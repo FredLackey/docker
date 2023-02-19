6a44feddbf7b27ac912dedc1063c8eb13c78d2ec
Fred Lackey <fred.lackey@gmail.com>
2023-02-19 11:47:04 -0500

    upgrade pgadmin to 6.20

--- images/pgadmin/build.sh
+++ images/pgadmin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t fredlackey/pgadmin:4-6.12 .
-docker build -t fredlackey/pgadmin:6.12 .
+docker build -t fredlackey/pgadmin:4-6.20 .
+docker build -t fredlackey/pgadmin:6.20 .
 docker build -t fredlackey/pgadmin:4 .
 docker build -t fredlackey/pgadmin:latest .
 docker build -t fredlackey/pgadmin .
